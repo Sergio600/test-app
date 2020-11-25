a92be2ca99d3ab43e32ddf2ccc71a403f755369d
Sergio <sergio600@mail.ru>
2020-11-25 21:22:32 +0300

    1
    1

---
+++ src/app/window/window.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Part1Component } from '../part1/part1.component';
+
+
+
+@NgModule({
+  declarations: [Part1Component],
+  imports: [
+    CommonModule
+  ]
+})
+export class WindowModule { }
